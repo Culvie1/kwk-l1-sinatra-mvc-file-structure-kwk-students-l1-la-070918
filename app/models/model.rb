5e02a20262aade8980ab695ebcec8b75bfc751c1
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-17 23:17:16 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
-class Model
-  # Replace with Dog clas
+#THIS IS WHERE THE RUBY HAPPENS
+def get_fortune
+  fortunes_array = ["You will have a great day", "You will achieve one of your goals today", "You will win the lottery", "Your crush may contact you", "You will be in an accident", "You will lose something today", "Something magical will happen today", "You will only drink Core water today"]
+  return fortunes_array.sample
 end
+
+puts get_fortune
